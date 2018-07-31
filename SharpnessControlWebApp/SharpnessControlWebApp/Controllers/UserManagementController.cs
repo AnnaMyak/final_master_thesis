78474f25237ed8c3acc0076a785f6b9229a0f318
AnnaMyak <myakinen@gmail.com>
2018-07-31 23:34:50 +0200

    Entity Management first operations

--- SharpnessControlWebApp/SharpnessControlWebApp/Controllers/UserManagementController.cs
+++ SharpnessControlWebApp/SharpnessControlWebApp/Controllers/UserManagementController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using IdentitySample.Models;
+using Sharpness.WebApp.Models.Sharpness_Persistence.Sharpness_Entities;
+using Sharpness.WebApp.Models.Sharpness_Persistence.Sharpness_Repositories.Implementation;
+using Sharpness.WebApp.Models.Sharpness_Persistence.Sharpness_Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +13,10 @@ namespace SharpnessControlWebApp.Controllers
 {
     public class UserManagementController : Controller
     {
-        // GET: UserManagement
-        public ActionResult Index()
+        ActionResult Index()
         {
             return View();
         }
+
     }
 }
