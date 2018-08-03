0bb211c7a25b425400f5e6b2fba5f935e13a5bca
AnnaMyak <myakinen@gmail.com>
2018-08-03 16:57:39 +0200

    pdf sharp added

---
+++ SharpnessControlWebApp/SharpnessControlWebApp/Extras/SharpnessPdfReport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using MigraDoc.DocumentObjectModel;
+
+namespace SharpnessControlWebApp.Extras
+{
+    public class SharpnessPdfReport
+    {
+
+        public void  generateSharpnessTestReport()
+        {
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+            gfx.DrawString("Hello, World!", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height),XStringFormats.Center);
+            const string filename = "HelloWorld.pdf";
+            document.Save(filename);
+            Process.Start(filename);
+
+        }
+        
+    }
+}
