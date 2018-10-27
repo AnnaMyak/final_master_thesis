b5ae4c8c1234dfa189651db089fb8f5724a08693
AnnaMyak <myakinen@gmail.com>
2018-10-27 23:16:54 +0200

    chart js, added scale option for bar charts

--- SharpnessControlWebApp/SharpnessControlWebApp/Controllers/HomeController.cs
+++ SharpnessControlWebApp/SharpnessControlWebApp/Controllers/HomeController.cs
@@ -1,15 +1,32 @@
-﻿using SharpnessControlWebApp.Extras;
+﻿using IdentitySample.Models;
+using SharpnessControlWebApp.Extras;
+using System;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace IdentitySample.Controllers
 {
     public class HomeController : Controller
     {
+
+        
+
         [HttpGet]
         public ActionResult Index()
         {
-            var test = new SharpnessPdfReport();
-            test.generateSharpnessTestReport();
+            //var test = new SharpnessPdfReport();
+            //test.generateSharpnessTestReport();
+            var dateFinnish = DateTime.Now.AddDays(20);
+            var random = new Random();
+            var context = new ApplicationDbContext();
+
+            for (int i = 1; i < 320; i++)
+            {
+                dateFinnish = dateFinnish.AddDays(-1);
+                //Console.WriteLine(dateFinnish.ToString() + " " + random.Next(4, 8).ToString());
+            }
+
+
             return View();
         }
 
